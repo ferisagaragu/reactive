a1f628b0c7cab043c952099af874f87581565acf
Fernando García <ferisagaragu@gmail.com>
2020-03-11 17:06:26 -0600

    integracion de objeto request

--- src/main/java/org/javabrain/example/Operations.java
+++
@@ -1,9 +0,0 @@
-package org.javabrain.example;
-
-public class Operations {
-
-	public static void saluda() {
-		System.out.println("hola papantlan");
-	}
-
-}
---
+++ src/main/java/org/javabrain/reactive/web/exception/BadRequestException.java
@@ -0,0 +1,24 @@
+package org.javabrain.reactive.web.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public class BadRequestException extends ResponseStatusException {
+
+	private String developMessage;
+
+
+	public BadRequestException(String reason) {
+		super(HttpStatus.BAD_REQUEST, reason);
+	}
+
+	public BadRequestException(String reason, String developMessage) {
+		super(HttpStatus.BAD_REQUEST, reason);
+		this.developMessage = developMessage;
+	}
+
+	public String getDevelopMessage() {
+		return developMessage;
+	}
+
+}
